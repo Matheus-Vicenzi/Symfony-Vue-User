6df09042aa04e6f5387643218ef296080350a57d
Matheus-Vicenzi <matheus.vicenzi2000@gmail.com>
2025-08-31 23:48:59 -0300

    production ready

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+console.log("API Base URL:", apiBaseUrl);
+
 export const httpPublic = axios.create({
-  baseURL: "/api",
+  baseURL: apiBaseUrl,
   headers: { "Content-Type": "application/json" },
 });
 
 export const httpAuth = axios.create({
-  baseURL: "/api",
+  baseURL: apiBaseUrl,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
